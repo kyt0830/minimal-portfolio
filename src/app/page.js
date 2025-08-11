c16a48f421ce198d1d8c680615d5af4d44db4eae
kyt0830 <kyt910830@gmail.com>
2025-08-11 12:34:11 +0900

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,70 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import { createClient } from '@/utils/supabase/client';
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+  const {data:projects, error} = await supabase.from("portfolio").select();
+
+  const getPublicURL = (path)=>{
+    const { data } = supabase
+    .storage
+    .from('portfolio')
+    .getPublicUrl(path);
+    return data.publicUrl;
+  }  
+
+
+
+  if(error){
+    console.log('데이터 조회 실패', error);
+    return <div>데이터 조회 실패</div>
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="container latest_portfolio">
+        <div className="row intro">
+            <div className="col-md-4">
+                <div className="contents shadow">
+                    <h2 className="heading2">I&apos;m alikerock</h2>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div className="contents shadow">
+                    <h2 className="heading2">I create super awesome stuff</h2>
+                </div>
+            </div>
+            <div className="col-md-4">
+                <div className="contents shadow">
+                    <h2 className="heading2">I&apos;m available for freelance projects</h2>
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="row list">
+          {
+            projects.map(item=> 
+              <div className="col-md-4" key={item.id}>
+                <div className="contents shadow">
+                    <Image src={getPublicURL(item.thumbnail)} width={364} height={209} alt={item.title} />
+                    <div className="hover_contents">
+                        <div className="list_info">
+                            <h3>
+                              <a href={`/project/${item.id}`}>{item.title}</a> 
+                              <Image src="/images/portfolio_list_arrow.png" width={6} height={8} alt="list arrow"/>
+                            </h3>
+                            <p><a href={`/project/${item.id}`}>Click to see project</a></p>
+                        </div>
+                    </div>
+                </div>
+              </div>          
+            )
+          }
+                     
+        </div>
+        <p className="porfolio_readmore">
+            <a href="" className="primary-btn">See my full portfolio</a>        
+        </p>
     </div>
+    </>
   );
 }
